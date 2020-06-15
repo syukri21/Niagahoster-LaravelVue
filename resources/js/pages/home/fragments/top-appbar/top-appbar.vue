6d1f50bba59fc9cffcf92caaac9371ac6de4ee63
myari211 <syukrihsb148@gmail.com>
2020-06-16 06:52:02 +0700

    fix top appbar

--- resources/js/pages/home/fragments/top-appbar/top-appbar.vue
+++ resources/js/pages/home/fragments/top-appbar/top-appbar.vue
@@ -1,22 +1,26 @@
 <template>
-    <Container>
-        <div class="flex justify-between">
-            <div>
-                <span class="text-xs">
-                    Gratis Ebook 9 Cara Cerdas Menggunakan Domain [x]
-                </span>
+    <div class="py-2 border-b border-gray-200">
+        <Container>
+            <div class="flex justify-between">
+                <div>
+                    <faw-icon icon="tag" class="text-secondary"></faw-icon>
+                    <span class="text-xs ml-1">
+                        Gratis Ebook 9 Cara Cerdas Menggunakan Domain
+                    </span>
+                    <span class="text-xs cursor-pointer">[x]</span>
+                </div>
+                <div>
+                    <Button icon="user-circle" size="sm">Member Area</Button>
+                    <Button icon="comments" class="ml-4" size="sm">
+                        Live Chat
+                    </Button>
+                    <Button icon="phone" class="ml-4" size="sm">
+                        0274-5305505
+                    </Button>
+                </div>
             </div>
-            <div>
-                <Button icon="user-circle" size="sm">Member Area</Button>
-                <Button icon="comments" class="ml-4" size="sm">
-                    Live Chat
-                </Button>
-                <Button icon="phone" class="ml-4" size="sm">
-                    0274-5305505
-                </Button>
-            </div>
-        </div>
-    </Container>
+        </Container>
+    </div>
 </template>
 
 <script src="./top-appbar.js"></script>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,7 @@
+const {
+    colors
+} = require('tailwindcss/defaultTheme')
+
 module.exports = {
     purge: {
         enabled: process.env.NODE_ENV === 'production',
@@ -27,21 +31,29 @@ module.exports = {
                 xl: "1150px"
             },
         },
-        colors: {
-            secondary: {
-                default: "#0090EF",
-                lighter: '#00A2F3',
-                dark: "3007FDE"
-            },
-            primary: {
-                default: "#3A4166"
-            },
-            background: {
-                primary: "#FFFFFF",
-                secondary: "303030",
-            },
-            success: "#00B348",
-        }
+        colors: theme => {
+            console.log("theme", theme("color"))
+
+            return ({
+                // ...theme('colors'),
+                gray: {
+                    ...colors.gray
+                },
+                secondary: {
+                    default: "#0090EF",
+                    light: '#00A2F3',
+                    dark: "#007FDE"
+                },
+                primary: {
+                    default: "#3A4166"
+                },
+                background: {
+                    primary: "#FFFFFF",
+                    secondary: "303030",
+                },
+                success: "#00B348",
+            })
+        },
     },
     variants: {},
     plugins: [
