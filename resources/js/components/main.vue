c62054d1d8f1314a0a1e9c174ff2904cc00de085
myari211 <syukrihsb148@gmail.com>
2020-06-15 12:28:29 +0700

    add vue

---
+++ resources/js/components/main.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        test
+    </div>
+</template>
+
+<script>
+export default {
+    console: () => {
+        console.log("new one");
+    },
+};
+</script>
+
+<style lang="css">
+div {
+    font-size: "20px";
+}
+</style>
