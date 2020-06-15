04a5ad9cf620b5049eceefb530a467452b99cd7c
myari211 <syukrihsb148@gmail.com>
2020-06-16 01:19:46 +0700

    fix some config on scss tailwinds

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+    purge: {
+        enabled: process.env.NODE_ENV === 'production',
+        content: [
+            './public/**/*.html',
+            './src/**/*.vue'
+        ],
+        options: {
+            whitelistPatterns: [
+                /-(leave|enter|appear)(|-(to|from|active))$/,
+                /^(?!(|.*?:)cursor-move).+-move$/,
+                /^router-link(|-exact)-active$/
+            ],
+        },
+    },
+    theme: {
+        extend: {},
+    },
+    variants: {},
+    plugins: [
+        require('autoprefixer'),
+        require('tailwindcss'),
+    ],
+}
